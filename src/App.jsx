e2d33233cf596df8821788fad980083728f84e66
Vatsaly76 <shuklavatsaly@gmail.com>
2026-02-01 16:12:52 +0530

    Update camera position in Canvas and adjust Dog component's primitive properties

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ import Dog from './components/Dog'
 const App = () => {
   return (
     <div className="app">
-      <Canvas camera={{ position: [2, 2, 3], fov: 50 }} shadows>
+      <Canvas camera={{ position: [0, 0, 1], fov: 50 }} shadows>
         <color attach="background" args={[ '#111111' ]} />
         <Suspense fallback={null}>
           <Dog />
--- src/components/Dog.jsx
+++ src/components/Dog.jsx
@@ -1,13 +1,15 @@
 import React from 'react'
+
 import { OrbitControls, useGLTF } from '@react-three/drei'
 const Dog = () => {
     const dog = useGLTF('/models/dog.drc.glb')
+    
     return (
         <>
             <ambientLight intensity={0.4} />
-            <primitive object={dog.scene} scale={0.5} position={[0, 0, 0]} />
+            <primitive object={dog.scene}  position={[0.1, -0.5, 0]} rotation={[0, Math.PI/4, 0]} />
             <directionalLight intensity={10} color={0xFFFFFF} position={[0, 5, 5]} />
-            <OrbitControls enableZoom={true} />
+            <OrbitControls />
 
         </>
     )
